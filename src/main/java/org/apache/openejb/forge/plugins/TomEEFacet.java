9eef588e874ac65e126a8375ba37589c8349236e
Romain Manni-Bucau <rmannibucau@apache.org>
2013-01-21 11:28:17 +0100

    correct usage of qualifier + adding help info in @Command

--- src/main/java/org/apache/openejb/forge/plugins/TomEEFacet.java
+++ src/main/java/org/apache/openejb/forge/plugins/TomEEFacet.java
@@ -1,13 +1,16 @@
 package org.apache.openejb.forge.plugins;
 
 import org.jboss.forge.maven.MavenPluginFacet;
+import org.jboss.forge.maven.plugins.Configuration;
 import org.jboss.forge.maven.plugins.ConfigurationElementImpl;
 import org.jboss.forge.maven.plugins.MavenPluginBuilder;
 import org.jboss.forge.project.dependencies.Dependency;
 import org.jboss.forge.project.dependencies.DependencyBuilder;
 import org.jboss.forge.project.facets.BaseFacet;
 import org.jboss.forge.project.facets.DependencyFacet;
+import org.jboss.forge.project.facets.ResourceFacet;
 import org.jboss.forge.shell.Shell;
+import org.jboss.forge.shell.plugins.RequiresFacet;
 
 import javax.inject.Inject;
 import java.util.Arrays;
@@ -15,6 +18,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@RequiresFacet({ DependencyFacet.class, ResourceFacet.class })
 public class TomEEFacet extends BaseFacet {
     public static final String GID = "org.apache.openejb.maven";
     public static final String AID = "tomee-maven-plugin";
@@ -28,22 +32,30 @@ public class TomEEFacet extends BaseFacet {
         final List<Dependency> versions = project.getFacet(DependencyFacet.class).resolveAvailableVersions(dep);
         final Dependency dependency = shell.promptChoiceTyped("What version do you want to install?", versions);
 
-        final String classifier = shell.promptChoiceTyped("Which distribution do you want to install?",
-                Arrays.asList("webprofile", "jaxrs", "plus"));
-        dep.setClassifier(classifier);
-
         final MavenPluginBuilder plugin = MavenPluginBuilder.create().setDependency(dependency);
+        final Configuration config = plugin.getConfig();
+
         {
+            final String classifier = shell.promptChoiceTyped("Which distribution do you want to install?",
+                    Arrays.asList("webprofile", "jaxrs", "plus"), "webprofile");
+
             final ConfigurationElementImpl classifierConfig = new ConfigurationElementImpl();
             classifierConfig.setName("tomeeClassifier");
             classifierConfig.setText(classifier);
-            plugin.getConfig().addConfigurationElement(classifierConfig);
+            config.addConfigurationElement(classifierConfig);
+        }
+        {
+            final String httpPort = shell.prompt("Which http port do you want to use?", "8080");
+            final ConfigurationElementImpl httpPortConfig = new ConfigurationElementImpl();
+            httpPortConfig.setName("tomeeHttpPort");
+            httpPortConfig.setText(httpPort);
+            config.addConfigurationElement(httpPortConfig);
         }
         if (isMoreThan(dependency, 1, 5, 2)) {
             final ConfigurationElementImpl simpleLog = new ConfigurationElementImpl();
             simpleLog.setName("simpleLog");
             simpleLog.setText("true");
-            plugin.getConfig().addConfigurationElement(simpleLog);
+            config.addConfigurationElement(simpleLog);
         }
 
         project.getFacet(MavenPluginFacet.class).addPlugin(plugin);
--- src/main/java/org/apache/openejb/forge/plugins/TomEEPlugin.java
+++ src/main/java/org/apache/openejb/forge/plugins/TomEEPlugin.java
@@ -1,7 +1,5 @@
 package org.apache.openejb.forge.plugins;
 
-import javax.enterprise.event.Event;
-import javax.inject.Inject;
 import org.jboss.forge.project.facets.events.InstallFacets;
 import org.jboss.forge.project.facets.events.RemoveFacets;
 import org.jboss.forge.shell.Shell;
@@ -10,6 +8,9 @@ import org.jboss.forge.shell.plugins.Command;
 import org.jboss.forge.shell.plugins.Help;
 import org.jboss.forge.shell.plugins.Plugin;
 import org.jboss.forge.shell.plugins.SetupCommand;
+
+import javax.enterprise.event.Event;
+import javax.inject.Inject;
 
 @Alias("tomee")
 @Help("A plugin helping with the tomee-maven-plugin usage")
@@ -33,27 +34,27 @@ public class TomEEPlugin implements Plugin {
         remove.fire(new RemoveFacets(TomEEFacet.class));
     }
 
-    @Command("run")
+    @Command(value = "run", help = "Start TomEE and wait for Ctrl+C to stop it")
     public void run() throws Exception {
-        command("run");
+        tomeeMvnPlugin("run");
     }
 
-    @Command("debug")
+    @Command(value = "debug", help = "Start TomEE in remote debug mode and wait for Ctrl+C to stop it")
     public void debug() throws Exception {
-        command("debug");
+        tomeeMvnPlugin("debug");
     }
 
-    @Command("start")
+    @Command(value = "start", help = "Start TomEE and forget it")
     public void start() throws Exception {
-        command("start");
+        tomeeMvnPlugin("start");
     }
 
-    @Command("stop")
+    @Command(value = "stop", help = "Stop TomEE (when started with start)")
     public void stop() throws Exception {
-        command("stop");
+        tomeeMvnPlugin("stop");
     }
 
-    private void command(final String cmd) throws Exception {
+    private void tomeeMvnPlugin(final String cmd) throws Exception {
         shell.execute("mvn tomee:" + cmd);
     }
 }
